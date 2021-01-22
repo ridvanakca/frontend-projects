3d2189ba5072650d15d95ab10c774d2741a2a645
Rıdvan Akca <akca.ridvann@gmail.com>
2021-01-22 13:23:24 +0300

    header is designed

--- my-angular-blog/src/app/core/header/header.component.ts
+++ my-angular-blog/src/app/core/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  @ViewChild('navMenu') navMenu: ElementRef;
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  toggleNavbar() {
+    this.navMenu.nativeElement.classList.toggle('is-active');
+  }
+  
 }
