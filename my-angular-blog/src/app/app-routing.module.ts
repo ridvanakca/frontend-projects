ce0ce6581c608231b3642b1960980ccf543dd1d1
Rıdvan Akca <akca.ridvann@gmail.com>
2021-02-15 22:05:53 +0300

    routing is updated

--- my-angular-blog/src/app/app-routing.module.ts
+++ my-angular-blog/src/app/app-routing.module.ts
@@ -1,14 +1,16 @@
-import { PostListComponent } from '../app/posts/post-list/post-list.component';
 import { ContactComponent } from './contact/contact.component';
 import { HomeComponent } from './home/home.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PostListComponent } from './posts/post-list/post-list.component';
+import { PostDetailComponent } from './posts/post-detail/post-detail.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'contact', component: ContactComponent },
   { path: '', pathMatch: 'full', redirectTo: '/home' },
-  
+  { path: 'blog', component: PostListComponent },
+  { path: 'blog/:id', component: PostDetailComponent}
 ];
 
 @NgModule({
--- my-angular-blog/src/app/posts/posts.module.ts
+++ my-angular-blog/src/app/posts/posts.module.ts
@@ -1,13 +1,10 @@
 import { RouterModule, Routes } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { PostDetailComponent } from './post-detail/post-detail.component';
 import { PostListComponent } from './post-list/post-list.component';
+import { PostDetailComponent } from './post-detail/post-detail.component';
 
-const routes: Routes= [
-  { path: 'blog', component: PostListComponent },
-  { path: 'blog/:id', component: PostDetailComponent}
-]
+
 
 @NgModule({
   declarations: [
@@ -16,7 +13,7 @@ const routes: Routes= [
   ],
   imports: [
     CommonModule,
-    RouterModule.forChild(routes)
+    RouterModule
   ]
 })
 export class PostsModule { }
